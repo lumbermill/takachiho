a50305fb4bcdee4ed84e3d47ce628f1a7a9d1f7f
Yosei Ito <y-itou@lumber-mill.co.jp>
2016-04-03 17:52:35 +0900

    デフォルト値を少し大きめに。

--- raspi/01-detector/etc/config.py
+++ raspi/01-detector/etc/config.py
@@ -5,5 +5,5 @@ FOLDER_PATH = os.environ["PROJECT_PATH"]+"/01-detector"
 PICTURE_NAME = "%y%m%d_%H%M%S.jpg"
 
 # 撮影間隔 high=センサー反応時 low=反応なし
-INTERVAL_HIGH = 10
-INTERVAL_LOW = 600
+INTERVAL_HIGH = 15
+INTERVAL_LOW = 900
