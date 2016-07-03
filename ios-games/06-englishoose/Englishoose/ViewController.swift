194fd1e17c000011aba39fcb8026c4ec3b77eb60
Yosei Ito <y-itou@lumber-mill.co.jp>
2016-07-03 13:22:16 +0900

    Englishooseのデータを外部化。

--- ios-games/06-englishoose/Englishoose/ViewController.swift
+++ ios-games/06-englishoose/Englishoose/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import iAd
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,ADBannerViewDelegate {
+    var drills:[Drill] = []
+    
+    @IBOutlet weak var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.drills = [Drill]()
+        // 問題の一覧と画像をダウンロードする（更新をしない限りはローカルのキャッシュ優先
+        Downloader.fetch_all({ (drills) in
+            self.drills = drills
+            self.table.reloadData()
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView,
+                     cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = table.dequeueReusableCellWithIdentifier("cell")!
+        let d = drills[indexPath.row]
+        cell.textLabel!.text = d.title
+        if(d.isValid()){
+            cell.detailTextLabel!.text = d.published_at+", "+d.author
+        }else{
+            cell.detailTextLabel!.text = String(d.options.count)+", "+d.published_at+", "+d.author
+        }
+        cell.imageView!.image = UIImage(contentsOfFile: Downloader.BASEDIR+d.options[0][0])
+        return cell
+    }
+    
+    func tableView(tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return drills.count
+    }
+    
+    @IBAction func refreshPushed(sender: AnyObject) {
+        Downloader.clear_all()
+        
+        self.drills = [Drill]()
+        Downloader.fetch_all({ (drills) in
+            self.drills = drills
+            self.table.reloadData()
+        })
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "start") {
+            let quizController:QuizController = segue.destinationViewController as! QuizController
+            let i = table.indexPathForSelectedRow!.row
+            quizController.loadDrill(drills[i])
+        }
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+    }
 }
 
