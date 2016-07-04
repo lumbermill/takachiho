660ce1dd46f0f032d6d7f135017a23a1652fcfe4
koki <gohannnotomo@gmail.com>
2016-07-04 14:52:28 +0900

    マッチングに使用した特徴点を後で確認できるようにするために元画像と白黒画像、特徴点データもデータとして保持するように修正した。 see #24

---
+++ webapps/Recognition-Tester/src/main/java/imgrecognition/Feature.java
@@ -0,0 +1,17 @@
+package imgrecognition;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+
+public class Feature {
+	public final Mat srcImage;
+	public final Mat grayImage;
+	public final Mat descriptors;
+	MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+
+	public Feature(Mat srcImage, Mat grayImage, MatOfKeyPoint keyPoints, Mat descriptors) {
+		this.srcImage = srcImage;
+		this.grayImage = grayImage;
+		this.keyPoints = keyPoints;
+		this.descriptors = descriptors;
+	}
+}
