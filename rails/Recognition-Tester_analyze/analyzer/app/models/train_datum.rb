7e496d8d06cbe7be78b4205f0813adcdd17ec657
koki <gohannnotomo@gmail.com>
2016-07-21 15:10:23 +0900

    [Recognition-Tester_analyze] 詳細画面にすべての訓練画像を出すようにした。

--- rails/Recognition-Tester_analyze/analyzer/app/models/train_datum.rb
+++ rails/Recognition-Tester_analyze/analyzer/app/models/train_datum.rb
@@ -1,10 +1,16 @@
 class TrainDatum < ApplicationRecord
+  IMAGE_BASE_PATH = "#{Rails.root}/app/assets/images/train-image"
+  IMAGE_BASE_URL  = "/assets/train-image"
   def image_count
-    image_pattern = "#{Rails.root}/app/assets/images/train-image/#{self.id}/*.jpg"
-    Dir.glob(image_pattern).count
+    all_image_paths.count
   end
 
   def label_image_path
-    "/assets/train-image/#{self.id}/0.jpg"
+    "#{IMAGE_BASE_URL}/#{self.id}/0.jpg"
+  end
+
+  def all_image_paths
+    image_pattern = "#{IMAGE_BASE_PATH}/#{self.id}/*.jpg"
+    Dir.glob(image_pattern).sort.map{|path| "#{IMAGE_BASE_URL}/#{self.id}/#{File.basename(path)}"}
   end
 end
