0e11eb08d05082a2d0ee93254d34fefadadce835
Yosei Ito <y-itou@lumber-mill.co.jp>
2016-08-03 22:53:42 +0900

    神社の名前だけ追加。どうやって座標を探すか？！

--- ios-games/07-takachiho-go/takachiho-go/Points.swift
+++ ios-games/07-takachiho-go/takachiho-go/Points.swift
@@ -16,14 +16,16 @@ struct Point {
     var kanji :String
     var lat :Double
     var lng :Double
+    var difficulty :Int = 1 // 1 to 3
     var visited_at :NSDate?
     var visited :Bool = false
     
-    init(_ name: String, kanji: String, lat: Double, lng: Double) {
+    init(_ name: String, kanji: String, lat: Double, lng: Double, difficulty: Int) {
         self.name = name
         self.kanji = kanji
         self.lat = lat
         self.lng = lng
+        self.difficulty = difficulty
         self.visited = has_photo()
     }
     
@@ -60,21 +62,31 @@ class Points {
     
     init(){
         
-        array += [Point("Kushifuru",kanji:"槵觸神社",lat: 32.710119,lng: 131.315543)]
-        array += [Point("Takachiho",kanji:"高千穂神社",lat: 32.706422,lng: 131.302074)]
-        array += [Point("Amanoiwato",kanji:"天岩戸神社",lat: 32.734211,lng: 131.350292)]
-        array += [Point("Futagami",kanji:"二上神社",lat: 32.686976,lng: 131.267234)]
-        array += [Point("Aratate",kanji: "荒立神社",lat: 32.711973,lng: 131.317138)]
-        array += [Point("Akimoto",kanji: "秋元神社",lat: 32.664870,lng: 131.295042)]
-        array += [Point("Sobodake",kanji: "祖母嶽神社",lat: 32.811019,lng: 131.278183)]
-        array += [Point("Nakahata",kanji: "中畑神社",lat: 32.723787,lng: 131.273881)]
-        array += [Point("Ishigami",kanji: "石神神社",lat: 32.706693,lng: 131.322702)]
-        array += [Point("Shimonohachiman Daijinja",kanji: "下野八幡大神社",lat: 32.745196,lng: 131.307631)]
-        array += [Point("Hachudairyuou Suijinja",kanji: "八大龍王水神社",lat: 32.730220,lng: 131.356878)]
+        array += [Point("Kushifuru",kanji:"槵觸神社",lat: 32.710119,lng: 131.315543,difficulty: 1)]
+        array += [Point("Takachiho",kanji:"高千穂神社",lat: 32.706422,lng: 131.302074,difficulty: 1)]
+        array += [Point("Amanoiwato",kanji:"天岩戸神社",lat: 32.734211,lng: 131.350292,difficulty: 1)]
+        array += [Point("Futagami",kanji:"二上神社",lat: 32.686976,lng: 131.267234,difficulty: 2)]
+        array += [Point("Aratate",kanji: "荒立神社",lat: 32.711973,lng: 131.317138,difficulty: 1)]
+        array += [Point("Akimoto",kanji: "秋元神社",lat: 32.664870,lng: 131.295042,difficulty: 3)]
+        array += [Point("Sobodake",kanji: "祖母嶽神社",lat: 32.811019,lng: 131.278183,difficulty: 3)]
+        array += [Point("Nakahata",kanji: "中畑神社",lat: 32.723787,lng: 131.273881,difficulty: 2)]
+        array += [Point("Ishigami",kanji: "石神神社",lat: 32.706693,lng: 131.322702,difficulty: 2)]
+        array += [Point("Shimonohachiman Daijinja",kanji: "下野八幡大神社",lat: 32.745196,lng: 131.307631,difficulty: 3)]
+        array += [Point("Hachudairyuou Suijinja",kanji: "八大龍王水神社",lat: 32.730220,lng: 131.356878,difficulty: 2)]
         // array += [Point("Akaishi Shrine",kanji: "赤石神社",lat: ,lng: )] どこ？
         // 逢初天神社 雲がかかって見えず…！
+        // 嶽宮神社 どこ？高千穂町押方325
+        // 熊野神社 ？高千穂町大字田原640
+        // 落立神社 ？岩戸2573
+        // 熊野鳴瀧神社 高千穂町河内32-2
+        // 芝原神社 ?押方３３７１-3
+        // 歳宮神社  高千穂町三田井川登
+        // 二ツ嶽神社 岩戸２１５
+        // 鉾神社 上岩戸１４５６
+        // 向山神社 向山中尾平1806
+        // 馬生木 八大之宮
         
-        array += [Point("NAZO",kanji: "謎の神社",lat: 32.717751,lng: 131.305815)]
+        array += [Point("NAZO",kanji: "謎の神社",lat: 32.717751,lng: 131.305815,difficulty: 2)]
         
         for p in array {
             dictionary[p.name] = p
