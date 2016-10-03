de8ce6651063e6ab5bfe010d782f12a97e9fc603
Yosei Ito <y-itou@lumber-mill.co.jp>
2016-10-04 00:20:13 +0900

    tmpl_logs/insertパスを追加(パラメタの調整これから)。

--- rails/mukoyama/config/routes.rb
+++ rails/mukoyama/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
     get 'tmpr_logs/graph/:raspi_id' => 'tmpr_logs#graph', as: :tmpr_logs_graph
     get 'tmpr_logs/graph_data'
+    get 'tmpr_logs/insert'
 
     resources :settings
     resources :addresses
