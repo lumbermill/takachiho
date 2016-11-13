cb8dc53cf1abd2c914f70b8eddb727dbb3bf5f1d
Yosei Ito <y-itou@lumber-mill.co.jp>
2016-11-13 13:47:59 +0900

    LINE BOT導入準備。 twilio.rb -> services.rb

--- rails/mukoyama/app/controllers/pages_controller.rb
+++ rails/mukoyama/app/controllers/pages_controller.rb
@@ -18,4 +18,40 @@ class PagesController < ApplicationController
 
   def usecase
   end
+
+  def line_client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def linebot
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless line_client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = line_client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          line_client.reply_message(event['replyToken'], message)
+          p event
+          #client.push_message(,message)
+        end
+      end
+    }
+
+    "OK"
+  end
 end
