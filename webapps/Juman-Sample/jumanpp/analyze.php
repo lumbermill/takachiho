c84fde6134dc472dbe3882946e8601f5e25e74c7
koki <gohannnotomo@gmail.com>
2016-11-29 14:08:13 +0900

    入力の半角文字をすべて全角に変換してから解析するようにした。 入力文字列に半角スペースと半角英数字が連続して存在する場合にPyKNPでエラーが発生するため。

--- webapps/Juman-Sample/jumanpp/analyze.php
+++ webapps/Juman-Sample/jumanpp/analyze.php
@@ -2,5 +2,6 @@
   $python = "/usr/bin/python";
   $analyze_script = "/home/ubuntu/src/takachiho/webapps/Juman-Sample/hyoki_yure.py";
   $input_text = $_REQUEST["input"];
+  $input_text = mb_convert_kana($input_text, "ASKV");
   system("echo '$input_text' | $python $analyze_script");
 
