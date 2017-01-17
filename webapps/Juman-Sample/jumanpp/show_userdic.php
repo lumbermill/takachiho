8e7cf08e84739c74918bbd793cd914facff9550e
koki <gohannnotomo@gmail.com>
2017-01-17 17:13:32 +0900

    ユーザ辞書の一覧ページを20語彙ずつにページ分けして表示するようにした。

--- webapps/Juman-Sample/jumanpp/show_userdic.php
+++ webapps/Juman-Sample/jumanpp/show_userdic.php
@@ -2,15 +2,21 @@
   $title="JUMAN++ユーザー辞書";
   include "./header.php";
   include "./common.php";
+  $row_of_page = 20;
+  $page = isset($_REQUEST['p']) ?  $_REQUEST['p'] : 1;
+  $start = ($page - 1) * $row_of_page + 1;
+  $end   = $start + $row_of_page - 1;
 ?>
 <div class="container">
   <h1><?= $title ?></h1>
   <p>独自に追加した語彙の一覧です。</p>
   <a href="./index.php">表記ゆれ検出に戻る</a>
+  <a href="./show_userdic.php?p=<?= $page - 1 ?>">前のページ</a>
+  <a href="./show_userdic.php?p=<?= $page + 1?>">次のページ</a>
   <div id="userdict">
     <pre><?php
       $midasi_script = "$script_dir/midasi.rb";
-      exec("cat $userdic_dir/* | sort -u | $ruby $midasi_script",$output);
+      exec("cat $userdic_dir/* | sort -u | sed -n '${start},${end}p'| $ruby $midasi_script",$output);
       foreach ($output as $out) {
         printf("\n%s:\n", $out);
         $word = explode(":", $out);
