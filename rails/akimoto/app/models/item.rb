7418e21e7798f945b870c17d18d73218811ea239
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-01-21 21:16:50 +0900

    とりあえず画像を見えるように

--- rails/akimoto/app/models/item.rb
+++ rails/akimoto/app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   has_many :tags
+
+  def picture_path
+    "/pictures/#{user_id}/#{code}-#{revision}.jpg"
+  end
+
+
+  def picture
+    "<img src='#{picture_path}' alt='' class='img img-responsive'/>".html_safe
+  end
 end
