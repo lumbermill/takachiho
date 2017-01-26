741035665f8fb2b394ffc6ca1ccf44f11c112f09
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-01-26 17:21:20 +0900

    最新画像の取得ができなくなっていた不具合に対応

--- rails/mukoyama/config/routes.rb
+++ rails/mukoyama/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 	get 'tmpr_logs/graph_data' => 'tmpr_logs#graph_data'
 	get 'logs/last_timestamp' => 'tmpr_logs#last_timestamp'
 
+  get 'pictures/request-upload' => 'pictures#request_upload'
 	get 'pictures/:raspi_id' => 'pictures#index', as: :pictures
 	get 'pictures/:raspi_id/:time_stamp' => 'pictures#show', as: :picture
 
@@ -26,7 +27,7 @@ Rails.application.routes.draw do
     resources :settings do
       member do
         get 'publish'
-        get 'unpublish' 
+        get 'unpublish'
       end
     end
     resources :addresses
@@ -38,7 +39,6 @@ Rails.application.routes.draw do
     get 'login-as' => 'users#login_as'
     get 'send_message' => 'addresses#send_message'
 
-    get 'pictures/request-upload' => 'pictures#request_upload'
     #get 'settings/:raspi_id/publish' => 'settings#publish'
   end
   # Pages
