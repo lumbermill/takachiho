7a8b7513d8592e04f6b170efbfb6979c013fad30
Koki Hashimoto <gohannnotomo@gmail.com>
2017-02-13 18:04:33 +0900

    [JUMAN++][未知語取得]取得したコンテンツを整形

--- webapps/Juman-Sample/dic_aggrigate/aggrigate.rb
+++ webapps/Juman-Sample/dic_aggrigate/aggrigate.rb
@@ -1,14 +1,10 @@
-# URLリストを読み込む
-# URLからコンテンツを取ってくる
-# <body>タグの内部だけを対象にする
-# コンテンツからタグを除去 str.gsub(/<\/?[^>]*>/
-# "。"を"。\n"に置換(１行が長すぎるとknpエラーになる)
-# 半角スペースを'/'に置換(knpエラー回避)
-# 半角'+'を全角'＋'に置換(knpエラー回避)
-# テンポラリファイルに保存
+
+require 'open-uri'
+
 def each_url_list
   Dir.glob("./url_lists/*") do |list|
     yield(list)
+    break
   end
 end
 
@@ -19,11 +15,22 @@ def each_url(url_list)
 end
 
 def get_contnet(url)
-puts url
+  open(url) do |content|
+    c = content.read
+    c.gsub!(/<head.+\/head>/m,"")         # <head>タグの中身を削除
+    c.gsub!(/<script.+\/script>/m,"")     # <script>タグの中身削除
+    c.gsub!(/<noscript.+\/noscript>/m,"") # <noscript>タグの中身を削除
+    c.gsub!(/<\/?[^>]*>/m,"")             # タグを全て除去
+    c.gsub!(/。/,"。\n")                  # "。"を"。\n"に置換(１行が長すぎるとknpエラーになる)
+    c.gsub!(/ /,"/")                      # 半角スペースを'/'に置換(knpエラー回避)
+    c.gsub!(/\+/,"＋")                    # 半角'+'を全角'＋'に置換(knpエラー回避)
+  end
 end
 
 each_url_list do |list|
   each_url(list) do |url|
-    get_contnet(url)
+    url.chomp!
+    puts get_contnet(url)
+# テンポラリファイルに保存
   end
 end
