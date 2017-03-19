7d7fb70829db7ff39cf41198f50bc8340f9fcfd5
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-03-19 09:10:40 +0900

    [mukoyama]デフォルトブランチをmasterに

--- rails/mukoyama/config/deploy.rb
+++ rails/mukoyama/config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'mukoyama'
 set :repo_url, 'https://github.com/lumbermill/takachiho.git'
 set :repo_tree, 'rails/mukoyama'
 
-set :branch, 'line-bot-awards'
+set :branch, 'master'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
