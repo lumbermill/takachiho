f1cc0369025f67dd7aa02f99a1bc59d27c1ac5c6
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-03-30 21:34:37 +0900

    Launched Jidori project.

---
+++ ios-games/08-jidori/08-jidori/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  08-jidori
+//
+//  Created by Yosei Ito on 2017/03/30.
+//  Copyright © 2017 LumberMill. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
