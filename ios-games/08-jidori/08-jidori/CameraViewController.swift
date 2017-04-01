1f9a087fd388b971d72bf32680b71dd0cb9f2839
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-04-02 08:26:10 +0900

    Building jidori app. Add some icons.

--- ios-games/08-jidori/08-jidori/CameraViewController.swift
+++ ios-games/08-jidori/08-jidori/CameraViewController.swift
@@ -7,17 +7,79 @@
 //
 
 import UIKit
+import AVFoundation
 
-class CameraController: UIViewController {
-
+class CameraViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate{
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var overlayView: OverlayView!
+    var session: AVCaptureSession!
+    var detector: CIDetector!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // TODO: frontとbackを切り替え
+        // TODO: シャッターを押すボタン付ける
+        // TODO: 被り物を選べるように？
+        let d = AVCaptureDevice.defaultDevice(withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.front)
+        
+        let cdi:AVCaptureDeviceInput
+        do {
+            cdi = try AVCaptureDeviceInput(device: d)
+        } catch {
+            NSLog("Can not initialize AVCaptureDeviceInput")
+            return
+        }
+        
+        let cvdo = AVCaptureVideoDataOutput()
+        cvdo.setSampleBufferDelegate(self, queue: DispatchQueue.main)
+        cvdo.alwaysDiscardsLateVideoFrames = true
+        session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSessionPresetMedium
+        
+        session.addInput(cdi)
+        session.addOutput(cvdo)
+        
+        detector = CIDetector(ofType: CIDetectorTypeFace,
+                              context: nil,
+                              options:[CIDetectorAccuracy: CIDetectorAccuracyLow])
+        
+        session.startRunning()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        if (session != nil) {
+            session.stopRunning()
+        }
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!,
+                       didOutputSampleBuffer sampleBuffer: CMSampleBuffer!,
+                       from connection: AVCaptureConnection!) {
+        connection.videoOrientation = .portrait
+        self.imageView.image = UIImageFromCMSamleBuffer(sampleBuffer)
+    }
+    
+    fileprivate func UIImageFromCMSamleBuffer(_ buffer:CMSampleBuffer)-> UIImage {
+        let pixelBuffer:CVImageBuffer = CMSampleBufferGetImageBuffer(buffer)!
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        
+        let options = [CIDetectorSmile : true, CIDetectorEyeBlink : true]
+        let image = UIImage(ciImage: ciImage)
+        self.overlayView.features = detector.features(in: ciImage, options: options)
+        self.overlayView.setNeedsDisplay()
+        
+        return image
+    }
 }
 
--- ios-games/08-jidori/CameraOverlayView.swift
+++ ios-games/08-jidori/CameraOverlayView.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import UIKit
+
+class OverlayView: UIView {
+    var features: [CIFeature]?
+    var at: CGAffineTransform!
+    var image: UIImage!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        at = CGAffineTransform(scaleX: 1, y: -1);
+        at = at.translatedBy(x: 0, y: -self.bounds.size.height);
+        image = UIImage(named: "checken1")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let fs = features else {
+            return
+        }
+        for f in fs {
+            let r = f.bounds.applying(at)
+            let ra = CGRect(x: r.origin.x - r.size.width / 2, y: r.origin.y - r.size.height,
+                            width: r.size.width * 2, height: r.size.height * 2)
+            image.draw(in: ra)
+            guard let ff = f as? CIFaceFeature else{
+                continue;
+            }
+            if ff.hasSmile {
+                NSString(string: "Smile!!").draw(at: r.origin, withAttributes: nil)
+                print("Smile!!")
+            }
+        }
+    }
+}
