cc889fc5d483e1fbe43d4646e8f9bb3370a32978
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-04-04 08:45:11 +0900

    [jidori]Add headgear and beak images.

--- ios-games/08-jidori/CameraOverlayView.swift
+++ ios-games/08-jidori/CameraOverlayView.swift
@@ -12,14 +12,16 @@ import UIKit
 class OverlayView: UIView {
     var features: [CIFeature]?
     var at: CGAffineTransform!
-    var image: UIImage!
+    var headgear: UIImage!
+    var beak: UIImage!
     
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         at = CGAffineTransform(scaleX: 1, y: -1);
         at = at.translatedBy(x: 0, y: -self.bounds.size.height);
-        image = UIImage(named: "checken1")
+        headgear = UIImage(named: "chicken1")
+        beak = UIImage(named: "beak1")
     }
     
     override func draw(_ rect: CGRect) {
@@ -30,13 +32,15 @@ class OverlayView: UIView {
             let r = f.bounds.applying(at)
             let ra = CGRect(x: r.origin.x - r.size.width / 2, y: r.origin.y - r.size.height,
                             width: r.size.width * 2, height: r.size.height * 2)
-            image.draw(in: ra)
+            headgear.draw(in: ra)
             guard let ff = f as? CIFaceFeature else{
                 continue;
             }
-            if ff.hasSmile {
-                NSString(string: "Smile!!").draw(at: r.origin, withAttributes: nil)
-                print("Smile!!")
+            if ff.hasMouthPosition {
+                let p = ff.mouthPosition.applying(at);
+                let w = ra.size.width/5;
+                let r = CGRect(x: p.x - w / 2.0, y: p.y - w / 2.0, width: w ,height: w)
+                beak.draw(in: r)
             }
         }
     }
