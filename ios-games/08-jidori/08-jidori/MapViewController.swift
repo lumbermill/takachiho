97dbbb148ae2e03652e63594daff8a538705d462
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-04-10 09:14:07 +0900

    [jidori]Add current location to MapView.

--- ios-games/08-jidori/08-jidori/MapViewController.swift
+++ ios-games/08-jidori/08-jidori/MapViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var targetButton: UIButton!
+    let lm = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        lm.requestWhenInUseAuthorization()
+        lm.startUpdatingLocation()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        lm.stopUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let l = locations.last {
+            if(targetButton.isEnabled == false){
+                let cr = MKCoordinateRegionMake(l.coordinate, MKCoordinateSpanMake(0.05, 0.05))
+                mapView.setRegion(cr, animated: true)
+                targetButton.isEnabled = true
+            }
+        }
+    }
+
+    @IBAction func targetButtonPushed(_ sender: AnyObject) {
+        targetButton.isEnabled = false
+    }
 
 }
 
