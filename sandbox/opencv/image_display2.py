23e9a497d2e01d03f52a413936bdda06bf3a1fae
Yosei Ito <yousei.itou@gmail.com>
2017-06-09 20:18:14 +0900

    Fix the paths to sample images.

--- sandbox/opencv/image_display2.py
+++ sandbox/opencv/image_display2.py
@@ -1,7 +1,7 @@
 import cv2
 from matplotlib import pyplot
 
-img = cv2.imread('dog.jpg',0)
+img = cv2.imread('samples/dog.jpg',0)
 
 pyplot.imshow(img, cmap='gray', interpolation='bicubic')
 pyplot.xticks([]), pyplot.yticks([])
