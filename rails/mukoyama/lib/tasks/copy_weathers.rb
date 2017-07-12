11d56f2859a999ae7606b0aa706e217bf7614d1d
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-07-12 12:08:31 +0900

    他ホスト(localhost以外)のデータベース接続に対応

--- rails/mukoyama/lib/tasks/copy_weathers.rb
+++ rails/mukoyama/lib/tasks/copy_weathers.rb
@@ -1,5 +1,9 @@
 # Copy weathers and weathers_cities records from the host.
-hostname = "ks05" # source host
+src_host = "ks05" # source host
+
+dst_user = Rails.configuration.database_configuration[Rails.env]["username"]
+dst_host = Rails.configuration.database_configuration[Rails.env]["host"] || "localhost"
+dst_db = Rails.configuration.database_configuration[Rails.env]["database"]
 
 require 'optparse'
 opt = OptionParser.new
@@ -10,18 +14,18 @@ opt.on('-e env') { |v| OPTS[:e] = v }
 OPTS = {}
 opt.parse!(ARGV)
 
-db = "mukoyama_" + (OPTS[:e] || "development")
+db = "-u#{dst_user} -h#{dst_host} #{dst_db}"
 
 # Not allowed to run on the source host.
-exit if ENV["HOSTNAME"] == hostname
+exit if ENV["HOSTNAME"] == src_host
 
 cmds = []
-cmds += ["ssh #{hostname} mysqldump -uroot k_exports weathers_cities | mysql #{db}"]
+cmds += ["ssh #{src_host} mysqldump -uroot k_exports weathers_cities | mysql #{db}"]
 if OPTS[:all]
-  cmds += ["ssh #{hostname} mysqldump -uroot k_exports weathers | mysql #{db}"]
+  cmds += ["ssh #{src_host} mysqldump -uroot k_exports weathers | mysql #{db}"]
 else
   # Fetch only latest(within a day) records.
-  cmds += ["ssh #{hostname} \"mysqldump -uroot k_exports weathers -t --replace --where 'modified_at >= date_add(now(),interval -1 day)'\" | mysql #{db}"]
+  cmds += ["ssh #{src_host} \"mysqldump -uroot k_exports weathers -t --replace --where 'modified_at >= date_add(now(),interval -1 day)'\" | mysql #{db}"]
 end
 cmds.each do |cmd|
   puts cmd
