cd03d9219f50faa97d9476d3485c375d280aaf21
koki <gohannnotomo@gmail.com>
2017-09-04 10:58:04 +0900

    BME280温度センサから取得した温度をさくらのIoTプラットフォームに送信するようにした。

--- rails/mukoyama/public/mukoyama_sakura.ino
+++ rails/mukoyama/public/mukoyama_sakura.ino
@@ -1,29 +1,53 @@
+// BME280 温度センサから温度の情報を取得し、送信するスケッチです
+// This prrogram requires SparkFun's BME280 library.
+// https://github.com/sparkfun/SparkFun_BME280_Arduino_Library
+#include "SparkFunBME280.h"
 #include <SakuraIO.h>
 
-// LM35 温度センサから温度の情報(アナログ)を取得し、送信するスケッチです
 
 SakuraIO_I2C sakuraio;
-
-float a_in = 0;
-float temp_c = 0;
+BME280 bme280;
 
 void setup(){
-  while((sakuraio.getConnectionStatus() & 0x80) != 0x80){
-    delay(1000);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Setup BME280
+  bme280.settings.commInterface = I2C_MODE; // Use I2C Interface
+  bme280.settings.I2CAddress = 0x76;        // I2C Address
+  bme280.settings.runMode = 3;              // Normal mode
+  bme280.settings.tempOverSample = 1;
+  bme280.settings.pressOverSample = 1;
+  bme280.settings.humidOverSample = 1;
+
+  delay(10);
+  if (bme280.begin() != 0x60)
+  {
+    Serial.println("BME280 communication error!");
+    while (1);
+  }
+
+  Serial.print("Waiting to come online");
+  for (;;) {
+    if ( (sakuraio.getConnectionStatus() & 0x80) == 0x80 ) break;
     Serial.print(".");
+    delay(1000);
   }
   Serial.println("");
 }
 
 void loop() {
-  a_in = analogRead(0);
-  temp_c = ((5 * a_in) / 1024) * 100;
-  sakuraio.enqueueTx(0,temp_c);
+  float temp = bme280.readTempC();
+  sakuraio.enqueueTx(0,temp);
   sakuraio.send();
+  Serial.print(temp);
+  Serial.print("  ");
+  Serial.print(sakuraio.getSignalQuality());
+  Serial.print("  ");
+  Serial.println(sakuraio.getConnectionStatus());
   // 15秒おきに送信しておいて、クラウド側から10分置きに適当なものをピックアップ
   // ESP8266同様のタイミング(NTP使用)でデータ送信できるようにすべきか？
   delay(15000);
-  Serial.print(temp_c);
-  Serial.print("  ");
-  Serial.println(sakuraio.getConnectionStatus());
 }
