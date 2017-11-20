7817cad16e07d7cbd56b29fa7ef1e3f30b22907c
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-11-20 17:12:31 +0900

    追加の分のweathermap用のcity_id 場所はとりあえずです。mukoyama独自に集信するようにします。

--- rails/mukoyama/config/initializers/mukoyama.rb
+++ rails/mukoyama/config/initializers/mukoyama.rb
@@ -1,3 +1,12 @@
 module Mukoyama
   DOWNLOAD_DIR = "/var/www/mukoyama/downloads"
+
+  # See http://openweathermap.org/help/city_list.txt for IDs.
+  CITY_IDS = {
+    Tokyo: 1850147,Osaka_shi: 1853909, Toyonaka: 1849837, Nishinomiya: 1855207,
+    Kashima_shi: 1860748, Tsukuba: 2110683, Saitama: 6940394, Nagoya: 1856057,
+    Miyazaki: 1856717, Nobeoka: 1855095, Hinokage:1862588, Owase: 1853514,
+    Gifu: 1863641, Tsu:1849796, Toyohashi: 1849846, Takayama: 1850892,
+    Yokkaichi: 1848373
+  }
 end
