23069df65b2ae4486c8d3ca71ab5f15e19692c4f
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-01-03 18:43:09 +0900

    [raspi]motionとGoogleDriveの連携手順

---
+++ raspi/08-motion/README.md
@@ -0,0 +1,95 @@
+# Motion
+RaspberryPiのカメラを使って動体検知を行い、撮影された画像をGoogle Driveに蓄積するデモです。
+motionのバージョンは4.0-1を想定しています。
+
+```
+sudo apt -y install motion
+```
+
+`/etc/modules` に以下の行を追加します。
+
+```
+bcm2835-v4l2
+```
+
+`/etc/defaults/motion` を編集してデーモンを有効化します。
+
+```
+start_motion_daemon=yes
+```
+
+`/etc/motion/motion.conf` の下記の設定を修正します。
+
+```
+width 640
+height 480
+event_gap 10
+output_pictures best
+ffmpeg_output_movies off
+snapshot_interval 1800
+locate_motion_mode on
+```
+
+修正内容は以下のようになります。他のパラメタも必要に応じて適宜調整してください。
+
+- 画像サイズを640x480に(320x240)
+- 動体を検知し続ける長さを10秒に(60秒)
+- 検知中に最も動きのあったフレームのみ保存(on:全て保存)
+- 検知中のムービー保管を無効化(有効)
+- 30分に1回スナップショットを保存(保存なし)
+- 動体を検知した範囲を矩形で囲む(囲まない)
+
+設定を完了したらRaspberryPiを再起動します。
+`/var/lib/motion` に画像(jpg)が蓄積され始めたら正常に動作しています。
+
+## GoogleDriveに画像を転送する
+
+必要なライブラリをインストールします。
+```
+sudo apt -y install python3-pip
+sudo pip3 install --upgrade httplib2 google-api-python-client
+```
+
+[Drive API クイックスタート](https://developers.google.com/drive/v3/web/quickstart/python)の
+手順を参考に関連づけたいGoogleアカウントの `client_secret.json` を入手してください。
+
+`motion-googledrive.py` と `client_secret.json` を以下のようにpiユーザの`bin`
+ディレクトリに配置します。
+
+```
+scp bin/motion-googledrive.py pi@raspberrypi.local:bin/motion-googledrive.py
+scp client_secret.json pi@raspberrypi.local:bin/motion-googledrive-secret.json
+```
+
+初回のみcredentialを取得するための手続きが必要です。以下のコマンドを実行して表示された
+URLにアクセスし、そこで得られたトークンを（続けて表示されている）プロンプトに貼り付けてください。
+`pictures-test` がDrive上に作成されるフォルダの名前になります。任意の文字列に変更可能です。
+`%f` は送信する画像ファイルのプレースホルダですが、%fのまま指定することで、初期化の動作をします。
+
+```
+sudo -u motion python3 /home/pi/bin/motion-googledrive.py --noauth_local_webserver pictures-test %f
+```
+
+この時に、Google Drive上に指定した名前のフォルダ(`pictures-test`)が生成されていれば接続成功です。
+
+`/etc/motion/motion.conf` の下記の設定を修正します（フォルダ名は上記で設定したものと合わせてください）。
+
+```
+on_picture_save python3 /home/pi/bin/motion-googledrive.py pictures-test %f
+```
+
+デーモンを再起動し、画像がDrive上に届くことを確認してください。
+
+```
+sudo service motion restart
+```
+
+## 古い画像を削除する
+画像は `/var/lib/motion` に蓄積されているので、適宜削除タスクを定義します。
+同ディレクトリ内の `.credentials` を削除してしまわないよう留意してください。
+
+```
+0 1 * * * sudo find /var/lib/motion/ -mtime +1 -name "*.jpg" -delete
+```
+
+TODO: drive上から一定期間で削除するには？
---
+++ raspi/08-motion/bin/motion-googledrive.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import httplib2
+import os, re, sys, time
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    p = argparse.ArgumentParser(parents=[tools.argparser])
+    p.add_argument("folder",help="Name of the folder on Google Drive")
+    p.add_argument("file",help="Path for the picture to post.")
+    flags = p.parse_args()
+except:
+    # TODO: fileパラメタが指定されていない場合、空のディレクトリを作って終了
+    # TODO: client_secretが存在しない場合、取得用のURLを出してあげる。
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+#SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = '/home/pi/bin/motion-googledrive-secret.json'
+APPLICATION_NAME = 'Motion for Google Drive'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'motion-googledrive.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def timestamp(filename):
+    """
+    Get timestamp from given filename(xxx-yyyymmddhhmmss.jpg).
+    Not used for now.
+    """
+    t = re.split('[-.]',filename)[1]
+    t = time.strptime(t,"%Y%m%d%H%M%S")
+    # TODO: How to take care of timezone?
+    return time.strftime("%Y-%m-%dT%H:%M:%S%z",t)
+
+def main():
+    """
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+    filepath = flags.file # saved file name
+    foldername = flags.folder
+    filename = os.path.basename(filepath)
+    delete_img = 'false'
+    print("local: %s" % (filepath))
+    print("remote: %s/%s" % (foldername,filename))
+
+    # Checks if the folder already exists and gets the ID.
+    items = service.files().list(q="name = '%s'" % (foldername)).execute().get('files')
+    if len(items) == 0:
+        body = {'name': foldername,'mimeType': "application/vnd.google-apps.folder"}
+        fid = service.files().create(body=body).execute().get('id')
+    else:
+        fid = items[0]['id']
+
+    if filepath == 'init' or filepath == '%f':
+        print("Initialized.")
+    else:
+        # Sends the picture.
+        body = { 'name': filename, 'parents':[fid] }
+        service.files().create(media_body=filepath, body=body).execute()
+        if delete_img == 'true':
+            print(filename+" deleted on local.")
+            os.remove(filepath)
+
+if __name__ == '__main__':
+    main()
