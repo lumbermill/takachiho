f808687575c27c065477a1a61c07253061d5bf4e
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-05-17 19:51:24 +0900

    [mukoyama]変数名を変更(setting->d)

--- rails/mukoyama/lib/tasks/temps-check.rb
+++ rails/mukoyama/lib/tasks/temps-check.rb
@@ -6,29 +6,29 @@ require 'uri'
 
 include ApplicationHelper  # for send_message
 
-Device.all.each do |setting|
-  log = Temp.where(device_id: setting.id).order(:updated_at).last
+Device.all.each do |d|
+  log = Temp.where(device_id: d.id).order(:updated_at).last
   next if log.nil?
   now = Time.now
-  print "device_id:#{setting.id} ts:#{log.updated_at} temp: #{log.temperature}"
-  msg = "#{now.hour}時#{now.min}分 #{setting.name} "
-  addresses = Address.where(device_id: setting.id, active: true)
+  print "device_id:#{d.id} ts:#{log.updated_at} temp: #{log.temperature}"
+  msg = "#{now.hour}時#{now.min}分 #{d.name} "
+  addresses = Address.where(device_id: d.id, active: true)
   if log.updated_at < now - Mailer::MAX_DELAY.minute
     puts " [too old]"
     msg += "データの受信を確認できません。最終受信日時は、#{log.updated_at.hour}時#{log.updated_at.min}分です。"
-  elsif setting.temp_max < log.temperature
+  elsif d.temp_max < log.temperature
     puts " [over]"
-    if setting.custom_msg_over.present?
+    if d.custom_msg_over.present?
       msg += "#{log.temperature}°Cです。設定を上回りました。"
     else
-      msg += setting.custom_msg_over
+      msg += d.custom_msg_over
     end
-  elsif setting.temp_min > log.temperature
+  elsif d.temp_min > log.temperature
     puts " [under]"
-    if setting.custom_msg_under.present?
+    if d.custom_msg_under.present?
       msg += "#{log.temperature}°Cです。設定を下回りました。"
     else
-      msg += setting.custom_msg_under
+      msg += d.custom_msg_under
     end
   else
     puts " [fine]"
