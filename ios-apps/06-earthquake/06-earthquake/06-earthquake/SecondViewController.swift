b5ac81a8342d65f73ef3ac29790d881340bda7b0
Yosei Ito <y-itou@lumber-mill.co.jp>
2019-01-20 18:54:14 +0900

    [earthquake]Initial commit.

---
+++ ios-apps/06-earthquake/06-earthquake/06-earthquake/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  06-earthquake
+//
+//  Created by Yosei Ito on 2019/01/20.
+//  Copyright © 2019 LmLab.net. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
