fcb5fe22fb8addb2da0c43f5b86866a73f9cd3a0
Yosei Ito <y-itou@lumber-mill.co.jp>
2019-02-11 17:56:55 +0900

    [earthquake]揺れの持続が閾値以上の場合、Slackに通知する

--- raspi/11-earthquake/motion.py
+++ raspi/11-earthquake/motion.py
@@ -3,13 +3,16 @@
 import time
 import numpy as np
 from envirophat import motion, leds
-
+import requests, json
 
 print("""This example will detect motion using the accelerometer.
 
 Press Ctrl+C to exit.
 
 """)
+
+SLACK_WEBHOOK = ENV["SLACK_WEBHOOK"]
+SLACK_THRESH = 30.0
 
 DETECT_THRESH = 0.01
 DETECT_NUM = 4
@@ -19,6 +22,15 @@ data = [[],[],[]]
 last_d = [-1,-1,-1]
 started = -1
 started_at = None
+
+## TODO: 揺れ終わり後、フーリエ変換を使って、震度を求める
+# 気象庁
+# https://www.data.jma.go.jp/svd/eqev/data/kyoshin/kaisetsu/calc_sindo.htm
+# FFT
+# https://momonoki2017.blogspot.com/2018/03/pythonfft-1-fft.html
+def stlength(data):
+    fx = np.abs(np.fft.fft(data[0]))[len(data[0])/2+1:]
+    return 1.0
 
 try:
     while True:
@@ -43,15 +55,14 @@ try:
         time.sleep(0.01)
         started -= 1
         if started == 0:
-            print("ended! %.3f" % ((time.time() - started_at)))
+            elapsed = time.time() - started_at
+            s = strength(data)
+            print("ended! %.2f %.2f" % (elapsed,s))
             started_at = None
             leds.off()
+            if elapsed > SLACK_THRESH:
+                requests.post(SLACK_WEBHOOK, data = json.dumps({
+                  'text': "An earthquake detected! %.2f %.2f" % (elapsed,s) ,
+                }))
 except KeyboardInterrupt:
     pass
-
-## TODO: 揺れ終わり後、フーリエ変換を使って、震度を求める
-# 気象庁
-# https://www.data.jma.go.jp/svd/eqev/data/kyoshin/kaisetsu/calc_sindo.htm
-# FFT
-# https://momonoki2017.blogspot.com/2018/03/pythonfft-1-fft.html
-fx = np.abs(np.fft.fft(data[0]))[len(data[0])/2+1:]
