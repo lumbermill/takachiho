f5b163b82b9ab1b6818c97f154d5f659be875f3f
Yosei Ito <y-itou@lumber-mill.co.jp>
2019-05-14 19:53:45 +0900

    [ios]Start creating demo app for in-app purchase.

---
+++ ios-apps/07-storekit/07-storekit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  07-storekit
+//
+//  Created by Yosei Ito on 2019/05/14.
+//  Copyright © 2019 LmLab.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
