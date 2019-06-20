65fd6216e723ec68d17aa551604057c00c49ccd8
afrin <afrin.deeba11114@gmail.com>
2019-06-20 13:17:17 +0900

    added animation of son

--- android-games/01-b-eater/app/src/main/java/com/lumbermill/b_eater/GameActivity.kt
+++ android-games/01-b-eater/app/src/main/java/com/lumbermill/b_eater/GameActivity.kt
@@ -1,12 +1,24 @@
 package com.lumbermill.b_eater
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_home.*
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var rocketAnimation: AnimationDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val image = findViewById<ImageView>(R.id.sonid).apply {
+            setBackgroundResource(R.drawable.son)
+            rocketAnimation = background as AnimationDrawable
+        }
+
+        rocketAnimation.start()
+
     }
 }
