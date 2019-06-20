71f5add3a96b8ab62fb4cd3e66a13493eb38df6b
afrin <afrin.deeba11114@gmail.com>
2019-06-20 17:48:32 +0900

    added start/stop button

--- android-games/01-b-eater/app/src/main/java/com/lumbermill/b_eater/GameActivity.kt
+++ android-games/01-b-eater/app/src/main/java/com/lumbermill/b_eater/GameActivity.kt
@@ -3,22 +3,54 @@ package com.lumbermill.b_eater
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.os.postDelayed
 import kotlinx.android.synthetic.main.activity_home.*
+import java.util.*
+import kotlin.random.Random
+import android.R.attr.delay
+import android.util.Log
+import androidx.core.os.HandlerCompat.postDelayed
+import androidx.core.os.HandlerCompat.postDelayed
+import kotlinx.android.synthetic.main.activity_game.*
+
 
 class GameActivity : AppCompatActivity() {
-    private lateinit var rocketAnimation: AnimationDrawable
+    private lateinit var sonAnimation: AnimationDrawable
+    private lateinit var fatherAnimation: AnimationDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        val image = findViewById<ImageView>(R.id.sonid).apply {
+        val sonImage = findViewById<ImageView>(R.id.sonid).apply {
             setBackgroundResource(R.drawable.son)
-            rocketAnimation = background as AnimationDrawable
+            sonAnimation = background as AnimationDrawable
+        }
+        
+
+//        sonAnimation.start()
+
+        val fatherImage = findViewById<ImageView>(R.id.fatherid).apply {
+            setBackgroundResource(R.drawable.father)
+            fatherAnimation = background as AnimationDrawable
+
         }
 
-        rocketAnimation.start()
+        val randomValues = Random.nextLong(0, 10)
+
+        startPlay.setOnClickListener {
+            sonAnimation.start()
+        }
+
+        stop.setOnClickListener {
+            sonAnimation.stop()
+        }
 
     }
+
+
+
 }
