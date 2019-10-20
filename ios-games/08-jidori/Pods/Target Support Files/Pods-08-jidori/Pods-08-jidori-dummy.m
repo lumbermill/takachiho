89365a6f347c70b803bd822a4067291279dad020
Yosei Ito <y-itou@lumber-mill.co.jp>
2019-10-20 17:08:58 +0900

    [jidori]pod install(Firebase)

---
+++ ios-games/08-jidori/Pods/Target Support Files/Pods-08-jidori/Pods-08-jidori-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_08_jidori : NSObject
+@end
+@implementation PodsDummy_Pods_08_jidori
+@end
--- ios-games/08-jidori/README.md
+++ ios-games/08-jidori/README.md
@@ -4,4 +4,4 @@ Jidori = 地鶏,自撮り = Chicken,Selfie
 
 https://lmlab.net/maps/jidori.json
 
-
+https://firebase.google.com/docs/ios/setup?authuser=1
